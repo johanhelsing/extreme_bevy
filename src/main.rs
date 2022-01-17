d7bda053e52773fd80ca6605b64985747c02992f
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-17 14:42:45 +0100

    Add keyboard movement

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use bevy::{prelude::*, render::camera::ScalingMode};
 
+#[derive(Component)]
+struct Player;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
@@ -14,6 +17,7 @@ fn main() {
         }))
         .insert_resource(ClearColor(Color::srgb(0.53, 0.53, 0.53)))
         .add_systems(Startup, (setup, spawn_player))
+        .add_systems(Update, move_player)
         .run();
 }
 
@@ -24,12 +28,45 @@ fn setup(mut commands: Commands) {
 }
 
 fn spawn_player(mut commands: Commands) {
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::srgb(0., 0.47, 1.),
-            custom_size: Some(Vec2::new(1., 1.)),
+    commands.spawn((
+        Player,
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::srgb(0., 0.47, 1.),
+                custom_size: Some(Vec2::new(1., 1.)),
+                ..default()
+            },
             ..default()
         },
-        ..default()
-    });
+    ));
+}
+
+fn move_player(
+    mut players: Query<&mut Transform, With<Player>>,
+    keys: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>,
+) {
+    let mut direction = Vec2::ZERO;
+    if keys.any_pressed([KeyCode::ArrowUp, KeyCode::KeyW]) {
+        direction.y += 1.;
+    }
+    if keys.any_pressed([KeyCode::ArrowDown, KeyCode::KeyS]) {
+        direction.y -= 1.;
+    }
+    if keys.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]) {
+        direction.x += 1.;
+    }
+    if keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
+        direction.x -= 1.;
+    }
+    if direction == Vec2::ZERO {
+        return;
+    }
+
+    let move_speed = 7.;
+    let move_delta = direction * move_speed * time.delta_seconds();
+
+    for mut transform in &mut players {
+        transform.translation += move_delta.extend(0.);
+    }
 }
