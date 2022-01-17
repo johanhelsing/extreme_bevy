7fc4722d0887f58aa84ea48d9185fdf397633046
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-17 10:31:56 +0100

    Setup camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = OrthographicCameraBundle::new_2d();
+    camera_bundle.orthographic_projection.scale = 1. / 50.;
+    commands.spawn_bundle(camera_bundle);
 }
