0b1f96a2e715a5760ec7b59155cb124d5ef7e759
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-17 10:51:23 +0100

    Add player and clear color

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
+        .insert_resource(ClearColor(Color::rgb(0.53, 0.53, 0.53)))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
+        .add_startup_system(spawn_player)
         .run();
 }
 
@@ -11,4 +13,15 @@ fn setup(mut commands: Commands) {
     let mut camera_bundle = OrthographicCameraBundle::new_2d();
     camera_bundle.orthographic_projection.scale = 1. / 50.;
     commands.spawn_bundle(camera_bundle);
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn_bundle(SpriteBundle {
+        sprite: Sprite {
+            color: Color::rgb(0., 0.47, 1.),
+            custom_size: Some(Vec2::new(1., 1.)),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
