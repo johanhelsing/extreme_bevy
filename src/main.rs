50dae218a19044f0737ada8ff114fc88c2d422c9
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-17 10:51:23 +0100

    Add player and clear color

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ fn main() {
             }),
             ..default()
         }))
-        .add_systems(Startup, setup)
+        .insert_resource(ClearColor(Color::srgb(0.53, 0.53, 0.53)))
+        .add_systems(Startup, (setup, spawn_player))
         .run();
 }
 
@@ -20,4 +21,15 @@ fn setup(mut commands: Commands) {
     let mut camera_bundle = Camera2dBundle::default();
     camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
     commands.spawn(camera_bundle);
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::srgb(0., 0.47, 1.),
+            custom_size: Some(Vec2::new(1., 1.)),
+            ..default()
+        },
+        ..default()
+    });
 }
