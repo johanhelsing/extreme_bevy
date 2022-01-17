6929850f52b1c9e135497be0bfa2395446854a38
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-17 10:31:56 +0100

    Setup camera

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, render::camera::ScalingMode};
 
 fn main() {
     App::new()
@@ -13,5 +13,12 @@ fn main() {
             }),
             ..default()
         }))
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(mut commands: Commands) {
+    let mut camera_bundle = Camera2dBundle::default();
+    camera_bundle.projection.scaling_mode = ScalingMode::FixedVertical(10.);
+    commands.spawn(camera_bundle);
 }
