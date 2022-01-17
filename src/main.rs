87cd07a9997a3fb38dec12a8615395f78daaa868
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-17 17:36:46 +0100

    Set up GGRS input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, render::camera::ScalingMode};
+use bevy::{prelude::*, render::camera::ScalingMode, utils::HashMap};
 use bevy_ggrs::*;
 use bevy_matchbox::prelude::*;
 
@@ -11,21 +11,32 @@ struct Player;
 // addresses are called `PeerId`s
 type Config = bevy_ggrs::GgrsConfig<u8, PeerId>;
 
+const INPUT_UP: u8 = 1 << 0;
+const INPUT_DOWN: u8 = 1 << 1;
+const INPUT_LEFT: u8 = 1 << 2;
+const INPUT_RIGHT: u8 = 1 << 3;
+const INPUT_FIRE: u8 = 1 << 4;
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                // fill the entire browser window
-                fit_canvas_to_parent: true,
-                // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
-                prevent_default_event_handling: false,
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    // fill the entire browser window
+                    fit_canvas_to_parent: true,
+                    // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                    prevent_default_event_handling: false,
+                    ..default()
+                }),
                 ..default()
             }),
-            ..default()
-        }))
+            GgrsPlugin::<Config>::default(),
+        ))
         .insert_resource(ClearColor(Color::srgb(0.53, 0.53, 0.53)))
         .add_systems(Startup, (setup, spawn_player, start_matchbox_socket))
-        .add_systems(Update, (move_player, wait_for_players))
+        .add_systems(Update, wait_for_players)
+        .add_systems(ReadInputs, read_local_inputs)
+        .add_systems(GgrsSchedule, move_player)
         .run();
 }
 
@@ -93,22 +104,57 @@ fn wait_for_players(mut commands: Commands, mut socket: ResMut<MatchboxSocket<Si
     commands.insert_resource(bevy_ggrs::Session::P2P(ggrs_session));
 }
 
+fn read_local_inputs(
+    mut commands: Commands,
+    keys: Res<ButtonInput<KeyCode>>,
+    local_players: Res<LocalPlayers>,
+) {
+    let mut local_inputs = HashMap::new();
+
+    for handle in &local_players.0 {
+        let mut input = 0u8;
+
+        if keys.any_pressed([KeyCode::ArrowUp, KeyCode::KeyW]) {
+            input |= INPUT_UP;
+        }
+        if keys.any_pressed([KeyCode::ArrowDown, KeyCode::KeyS]) {
+            input |= INPUT_DOWN;
+        }
+        if keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
+            input |= INPUT_LEFT
+        }
+        if keys.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]) {
+            input |= INPUT_RIGHT;
+        }
+        if keys.any_pressed([KeyCode::Space, KeyCode::Enter]) {
+            input |= INPUT_FIRE;
+        }
+
+        local_inputs.insert(*handle, input);
+    }
+
+    commands.insert_resource(LocalInputs::<Config>(local_inputs));
+}
+
 fn move_player(
     mut players: Query<&mut Transform, With<Player>>,
-    keys: Res<ButtonInput<KeyCode>>,
+    inputs: Res<PlayerInputs<Config>>,
     time: Res<Time>,
 ) {
     let mut direction = Vec2::ZERO;
-    if keys.any_pressed([KeyCode::ArrowUp, KeyCode::KeyW]) {
+
+    let (input, _) = inputs[0];
+
+    if input & INPUT_UP != 0 {
         direction.y += 1.;
     }
-    if keys.any_pressed([KeyCode::ArrowDown, KeyCode::KeyS]) {
+    if input & INPUT_DOWN != 0 {
         direction.y -= 1.;
     }
-    if keys.any_pressed([KeyCode::ArrowRight, KeyCode::KeyD]) {
+    if input & INPUT_RIGHT != 0 {
         direction.x += 1.;
     }
-    if keys.any_pressed([KeyCode::ArrowLeft, KeyCode::KeyA]) {
+    if input & INPUT_LEFT != 0 {
         direction.x -= 1.;
     }
     if direction == Vec2::ZERO {
