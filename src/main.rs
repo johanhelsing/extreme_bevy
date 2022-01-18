02972adcc891855a6ce82c048c2004b85abadb19
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-01-18 15:04:10 +0100

    Don't allow players to walk off map

--- src/main.rs
+++ src/main.rs
@@ -165,7 +165,13 @@ fn move_players(
 
         let move_speed = 7.;
         let move_delta = direction * move_speed * time.delta_secs();
-        transform.translation += move_delta.extend(0.);
+
+        let old_pos = transform.translation.xy();
+        let limit = Vec2::splat(MAP_SIZE as f32 / 2. - 0.5);
+        let new_pos = (old_pos + move_delta).clamp(-limit, limit);
+
+        transform.translation.x = new_pos.x;
+        transform.translation.y = new_pos.y;
     }
 }
 
