13ec980f529d7ae65846cdf57b24592e00a6cecd
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-10-27 12:40:19 +0200

    Resize canvas to browser window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                // fill the entire browser window
+                // TODO: re-enable in Bevy 0.14
+                // fit_canvas_to_parent: true,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
