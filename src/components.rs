571b44e533ccc4c1e8af3a6a979230f0274295a0
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2023-11-16 10:40:55 +0100

    Add checksumming for transforms

--- src/components.rs
+++ src/components.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use std::hash::{DefaultHasher, Hash, Hasher};
 
 #[derive(Component)]
 pub struct Player {
@@ -13,3 +14,25 @@ pub struct Bullet;
 
 #[derive(Component, Clone, Copy)]
 pub struct MoveDir(pub Vec2);
+
+pub fn checksum_transform(transform: &Transform) -> u64 {
+    let mut hasher = DefaultHasher::new();
+
+    assert!(
+        transform.is_finite(),
+        "Hashing is not stable for NaN f32 values."
+    );
+
+    transform.translation.x.to_bits().hash(&mut hasher);
+    transform.translation.y.to_bits().hash(&mut hasher);
+    transform.translation.z.to_bits().hash(&mut hasher);
+
+    transform.rotation.x.to_bits().hash(&mut hasher);
+    transform.rotation.y.to_bits().hash(&mut hasher);
+    transform.rotation.z.to_bits().hash(&mut hasher);
+    transform.rotation.w.to_bits().hash(&mut hasher);
+
+    // skip transform.scale as it's not used for gameplay
+
+    hasher.finish()
+}
