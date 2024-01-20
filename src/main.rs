47174a0610ae706d1e2d5a284451f35d7c1e0a58
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2024-01-20 15:46:11 +0100

    Use Xoshiro256PlusPlus rng seeded by score

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,8 @@ use bevy_roll_safe::prelude::*;
 use clap::Parser;
 use components::*;
 use input::*;
-use rand::Rng;
+use rand::{Rng, SeedableRng};
+use rand_xoshiro::Xoshiro256PlusPlus;
 
 mod args;
 mod components;
@@ -197,6 +198,7 @@ fn spawn_players(
     mut commands: Commands,
     players: Query<Entity, With<Player>>,
     bullets: Query<Entity, With<Bullet>>,
+    scores: Res<Scores>,
 ) {
     info!("Spawning players");
 
@@ -208,7 +210,7 @@ fn spawn_players(
         commands.entity(bullet).despawn_recursive();
     }
 
-    let mut rng = rand::thread_rng();
+    let mut rng = Xoshiro256PlusPlus::seed_from_u64((scores.0 + scores.1) as u64);
     let half = MAP_SIZE as f32 / 2.;
     let p1_pos = Vec2::new(rng.gen_range(-half..half), rng.gen_range(-half..half));
     let p2_pos = Vec2::new(rng.gen_range(-half..half), rng.gen_range(-half..half));
