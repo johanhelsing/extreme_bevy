48d2295be18fe008d23a03a22aad509723f7d665
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2024-03-15 10:21:35 +0100

    Don't hijack keyboard shortcuts

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main() {
             primary_window: Some(Window {
                 // fill the entire browser window
                 fit_canvas_to_parent: true,
+                // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                prevent_default_event_handling: false,
                 ..default()
             }),
             ..default()
