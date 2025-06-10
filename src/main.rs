896f7692879856e53b04fb622e412e51593a9512
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2025-06-10 18:32:29 +0200

    Set texture filtering to nearest

--- src/main.rs
+++ src/main.rs
@@ -63,16 +63,18 @@ fn main() {
 
     App::new()
         .add_plugins((
-            DefaultPlugins.set(WindowPlugin {
-                primary_window: Some(Window {
-                    // fill the entire browser window
-                    fit_canvas_to_parent: true,
-                    // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
-                    prevent_default_event_handling: false,
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        // fill the entire browser window
+                        fit_canvas_to_parent: true,
+                        // don't hijack keyboard shortcuts like F5, F6, F12, Ctrl+R etc.
+                        prevent_default_event_handling: false,
+                        ..default()
+                    }),
                     ..default()
-                }),
-                ..default()
-            }),
+                })
+                .set(ImagePlugin::default_nearest()),
             GgrsPlugin::<Config>::default(),
             RollbackSchedulePlugin::new_ggrs(),
             EguiPlugin::default(),
